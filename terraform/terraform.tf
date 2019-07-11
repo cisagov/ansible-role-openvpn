03f09f002b518d2bd20ae9b8e9d4b28dd3623972
Felddy <markf@geekpad.com>
2019-07-10 21:34:12 -0400

    The bucket name changes depending on the account.  Cool?

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     encrypt        = true
-    bucket         = "ncats-terraform-state-storage"
+    bucket         = "playground-terraform-state-storage"
     dynamodb_table = "terraform-state-lock"
     region         = "us-east-1"
     key            = "ansible-role-openvpn/terraform.tfstate"
