447c10f4d1ddef10eb2b170782b169bfe767409a
Felddy <markf@geekpad.com>
2019-07-18 13:22:33 -0400

    Fix script so it works correctly with different openssl and linux

--- files/fetch_certs.sh
+++ files/fetch_certs.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Download the DHS certificate authority certificates as well as the
 # intermediates, and root to which they are chained.  Each certificate
@@ -15,13 +15,18 @@ curl --insecure --silent https://pki.treas.gov/dhsca_fullpath.p7b | \
 openssl pkcs7 -print_certs -inform der -out dhsca_fullpath.pem
 
 # split chain of certificates into individual files
-split -a1 -p "subject" dhsca_fullpath.pem cert-
-
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # MacOS X
+  split -a1 -p "subject" dhsca_fullpath.pem cert-
+else
+  # Assume linux-gnu
+  csplit --prefix cert- --elide-empty-files --quiet dhsca_fullpath.pem /subject/ "{*}"
+fi
 # rename each file to subject_expiration.pem
 for filename in cert-*; do
   [ -e "$filename" ] || continue
   # get the last element of the subject
-  subject=$(openssl x509 -in "${filename}" -noout -subject | awk -F= '{print $NF}' | tr ' ' '_')
+  subject=$(openssl x509 -in "${filename}" -noout -subject | sed 's/ = /=/g' | awk -F= '{print $NF}' | tr ' ' '_')
   # get expiration year
   expire_year=$(openssl x509 -in "${filename}" -noout -enddate | awk '{print $4}')
   mv "${filename}" "${subject}_${expire_year}.pem"
