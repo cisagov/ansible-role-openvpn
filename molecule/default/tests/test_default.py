a9c6b3531f1866017ef26f6d9f1f71729faf79c0
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-06-25 13:55:24 -0400

    Prefer unit file drop-ins to editing the unit file provided by the package manager
    Resolves #80.  Using a unit file drop-in is the preferred method in
    the systemd documentation:
    https://www.freedesktop.org/software/systemd/man/latest/systemd.unit.html#Examples

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,6 +1,7 @@
 """Module containing the tests for the default scenario."""
 
 # Standard Python Libraries
+import configparser
 import os
 
 # Third-Party Libraries
@@ -17,3 +18,20 @@ def test_sysctl_settings(host, setting):
     """Test that sysctl values were set properly."""
     for key in setting:
         assert setting[key] == host.sysctl(key)
+
+
+@pytest.mark.parametrize(
+    "section,option,value",
+    [
+        ("Unit", "After", "network.target multi-user.target cloud-final.service"),
+        ("Service", "PrivateTmp", "False"),
+    ],
+)
+def test_openvpn_unit_modifications(host, section, option, value):
+    """Test that OpenVPN unit has been modified as expected."""
+    cmd = host.run("systemctl cat openvpn-server@primary.service")
+    assert cmd.rc == 0
+    config = configparser.ConfigParser(strict=False)
+    config.read_string(cmd.stdout)
+    assert config[section][option]
+    assert config[section][option] == value
