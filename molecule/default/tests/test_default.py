4371612d2465b9a12b28e68b0cbde74b4a59e69d
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-06-27 10:27:05 -0400

    Specify a type for the value of the unit file attribute when testing
    Ansible roles use YML (which has a notion of types) and systemd unit
    files use INI format (which does not and treats all values as
    strings).  @mcdonnnj noted in
    https://github.com/cisagov/ansible-role-openvpn/pull/81#discussion_r1653429157
    that there was some confusion when I wrote a (Boolean) value of
    "false" in tasks/main.yml but read back a (string) value of "False" in
    the test code.  To make things more explicit, I now specify a type for
    the systemd unit file attributes that I am testing.
    configparser.ConfigParser supports interpretation of values as
    strings, integers, floating-point numbers, or Booleans.
    
    The reason for the discrepancy that @mcdonnnj observed is that:
    1. Ansible's community.general.ini_file writes the YML value "false"
    to the unit file as the string "False".
    2. Python's configparser.ConfigParser.get() method interprets all
    values as strings, and therefore returns the string "False".
    
    Now what happens is:
    1. Ansible's community.general.ini_file writes the YML value "false"
    to the unit file as the string "False".
    2. Python's configparser.ConfigParser.getboolean() method interprets all
    values as Booleans, and therefore interprets the string "False" as a
    false Boolean value.  See
    https://docs.python.org/3/library/configparser.html#configparser.ConfigParser.getboolean
    for more details.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -20,18 +20,36 @@ def test_sysctl_settings(host, setting):
         assert setting[key] == host.sysctl(key)
 
 
+# The "type" string must correspond to a get*(section, option,...)
+# method of the ConfigParser class.  Options for the "type" string
+# are:
+# - "", which results in a call to get() and returns a string value
+# - "boolean" which results in a call to getboolean() and returns a
+#   Boolean value
+# - "int" which results in a call to getint() and returns an integer
+#   value
+# - "float" which results in a call to getfloat() and returns a
+#   floating-point value
+#
+# See here for more details:
+# https://docs.python.org/3/library/configparser.html#configparser.ConfigParser.get
 @pytest.mark.parametrize(
-    "section,option,value",
+    "section,option,value,value_type",
     [
-        ("Unit", "After", "network.target multi-user.target cloud-final.service"),
-        ("Service", "PrivateTmp", "False"),
+        ("Unit", "After", "network.target multi-user.target cloud-final.service", ""),
+        ("Service", "PrivateTmp", False, "boolean"),
     ],
 )
-def test_openvpn_unit_modifications(host, section, option, value):
+def test_openvpn_unit_modifications(host, section, option, value, value_type):
     """Test that OpenVPN unit has been modified as expected."""
     cmd = host.run("systemctl cat openvpn-server@primary.service")
     assert cmd.rc == 0
     config = configparser.ConfigParser(strict=False)
     config.read_string(cmd.stdout)
-    assert config[section][option]
-    assert config[section][option] == value
+
+    # Now we dynamically construct the method of ConfigParser that we
+    # want to call depending on the value of "type".
+    method_name = f"get{value_type}"
+    assert hasattr(config, method_name)
+    assert callable(method := getattr(config, method_name))
+    assert method(section, option) == value
